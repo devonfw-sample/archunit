038c95cb3efe12e2a01950a2cbd17edb7e1ec244
Nicolas van Bellen <nicolas.van.bellen@rwth-aachen.de>
2023-01-16 21:15:12 +0100

    first input for ComponentRulesTest

---
+++ src/test/java/com/devonfw/sample/archunit/ComponentRulesTest.java
@@ -0,0 +1,60 @@
+package com.devonfw.sample.archunit;
+
+import static com.tngtech.archunit.library.Architectures.layeredArchitecture;
+
+import com.tngtech.archunit.core.importer.ImportOption;
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.lang.ArchRule;
+
+/**
+ * JUnit test that validates the component rules architecture of this application.
+ */
+@AnalyzeClasses(packages = "com.devonfw.sample.archunit", importOptions = ImportOption.DoNotIncludeTests.class)
+public class ComponentRulesTest {
+
+  @ArchTest
+  private static final ArchRule DevonArchitectureComponentDeclarationCheck = //
+      layeredArchitecture().consideringAllDependencies() //
+          .layer("common").definedBy("..common..") //
+          .layer("logic").definedBy("..logic..") //
+          .layer("dataaccess").definedBy("..dataaccess..") //
+          .layer("service").definedBy("..service..") //
+          .layer("batch").definedBy("..batch..") //
+          .layer("ui").definedBy("..ui..") //
+          // TODO
+          .because("Dependency of technical layers violates architecture rules.");
+
+  @ArchTest
+  private static final ArchRule DevonArchitectureComponentDependencyCheck = //
+      layeredArchitecture().consideringAllDependencies() //
+          .layer("common").definedBy("..common..") //
+          .layer("logic").definedBy("..logic..") //
+          .layer("dataaccess").definedBy("..dataaccess..") //
+          .layer("service").definedBy("..service..") //
+          .layer("batch").definedBy("..batch..") //
+          .layer("ui").definedBy("..ui..") //
+          // TODO
+          .because("Dependency of technical layers violates architecture rules.");
+  
+  @ArchTest
+  private static final ArchRule DevonArchitectureLayerService2Service4ComponentCheck = //
+      layeredArchitecture();
+
+  @ArchTest
+  private static final ArchRule DevonArchitectureLayerService2Logic4ComponentCheck = //
+      layeredArchitecture();
+
+  @ArchTest
+  private static final ArchRule DevonArchitectureLayerLogic2Dataaccess4ComponentCheck = //
+      layeredArchitecture();
+
+  @ArchTest
+  private static final ArchRule DevonArchitectureLayerDataaccess2Dataaccess4ComponentCheck = //
+      layeredArchitecture();
+
+  @ArchTest
+  private static final ArchRule DevonArchitectureLayerBatch2Logic4ComponentCheck = //
+      layeredArchitecture();
+
+}
