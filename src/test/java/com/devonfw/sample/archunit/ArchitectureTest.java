c647d352d75a77e60778a8f00950e8631d6ca17b
Vladislav Sehtman <vlad96@hotmail.de>
2023-01-22 12:49:18 +0100

    Added Architecture Layer Rules 1-12.

--- src/test/java/com/devonfw/sample/archunit/ArchitectureTest.java
+++ src/test/java/com/devonfw/sample/archunit/ArchitectureTest.java
@@ -21,10 +21,40 @@ public class ArchitectureTest {
           .layer("dataaccess").definedBy("..dataaccess..") //
           .layer("service").definedBy("..service..") //
           .layer("batch").definedBy("..batch..") //
-          .layer("ui").definedBy("..ui..") //
+          .layer("ui").definedBy("..ui..")
+          .layer("java").definedBy("..java..") //
+          .layer("javax").definedBy("..javax..")
+          .layer("mapper").definedBy("..mapstruct..")
+          .layer("logger").definedBy("..org.slf4j..")
+          .layer("database").definedBy("..com.querydsl..")
+          .layer("spring").definedBy("..org.springframework..")
+          .layer("client").definedBy("..client")
+          
           // TODO
+          //.whereLayer("ui").mayNotBeAccessedByAnyLayer()
+          .whereLayer("common").mayOnlyAccessLayers("java", "javax") 
+          // L01: Common Layer doesnt depend on any other layer (except obv. java classes)
+          .whereLayer("client").mayOnlyBeAccessedByLayers("client", "java") 
+          // L02: verifying that only client layer code may depend on client layer.
+          .whereLayer("client").mayOnlyAccessLayers("client", "java") 
+          // L03: verifying that client layer does not depend on logic layer.
+          // L04: verifying that client layer does not depend on dataaccess layer.
+          // L05: verifying that client layer does not depend on batch layer.
+          .whereLayer("service").mayOnlyBeAccessedByLayers("ui", "client")
+          .whereLayer("service").mayOnlyAccessLayers("logic", "java", "javax", "common")
+          // L08: verifying that code from service layer does not depend on dataaccess layer.
+          // L06: verifying that service layer does not on depend batch layer.
+          .whereLayer("batch").mayOnlyAccessLayers("java")
+          // L07: verifying that batch layer does not depend on service layer.
+          // L11: verifying that batch layer does not depend on dataaccess layer.
+          .whereLayer("logic").mayOnlyBeAccessedByLayers("service") 
+          .whereLayer("logic").mayOnlyAccessLayers("dataaccess", "java", "javax", "mapper", "common", "logger")
+          // L09: verifying that code from logic layer does not depend on service layer (of same app).
+          .whereLayer("dataaccess").mayOnlyBeAccessedByLayers("logic")
+          .whereLayer("dataaccess").mayOnlyAccessLayers("dataaccess", "java", "javax", "common", "spring", "database")
+          // L10: verifying that dataaccess layer does not depend on service layer.
+          // L12: verifying that dataaccess layer does not depend on logic layer.
           .because("Dependency of technical layers violates architecture rules.");
-
   // ...
 
 }
