201311a984de87ed19c07a6f0ce69535e089fb54
zchriste <zina.christensen@capgemini.com>
2023-01-25 10:13:13 +0100

    implemented naming convention tests N3 & N4

---
+++ src/test/java/com/devonfw/sample/archunit/NamingConventionTest.java
@@ -0,0 +1,42 @@
+package com.devonfw.sample.archunit;
+
+import com.devonfw.sample.archunit.general.common.AbstractEto;
+import com.devonfw.sample.archunit.general.dataaccess.ApplicationPersistenceEntity;
+import com.tngtech.archunit.core.importer.ImportOption;
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.lang.ArchRule;
+
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
+
+/**
+ * JUnit test that validates the naming convention rules of this application.
+ */
+@AnalyzeClasses(packages = "com.devonfw.sample.archunit", importOptions = ImportOption.DoNotIncludeTests.class)
+public class NamingConventionTest {
+
+
+    /**
+     * DevonNamingConventionCheck verifying that classes extending ApplicationPersistenceEntity are following the
+     * naming convention by ending with 'Entity'.
+     */
+    @ArchTest
+    private static final ArchRule DevonNamingConventionEntityCheck =
+            classes()
+                    .that().areAssignableTo(ApplicationPersistenceEntity.class)
+                    .should().haveSimpleNameEndingWith("Entity")
+                    .because("Classes extending ApplicationPersistenceEntity must follow the naming convention by" +
+                            "ending with 'Entity'.");
+
+    /**
+     * DevonNamingConventionCheck verifying that classes extending AbstractEto are following the naming convention by
+     * ending with Eto.
+     */
+    @ArchTest
+    private static final ArchRule DevonNamingConventionEtoCheck =
+            classes()
+                    .that().areAssignableTo(AbstractEto.class)
+                    .should().haveSimpleNameEndingWith("Eto")
+                    .because("Classes extending AbstractEto must follow the naming convention by ending with 'Eto'.");
+
+}
