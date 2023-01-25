58e3579d1d9a8358ff5e16ece21bbcf9071d70aa
zchriste <zina.christensen@capgemini.com>
2023-01-25 10:21:03 +0100

    implemented naming convention violations

---
+++ src/main/java/com/devonfw/sample/archunit/task/archunitviolations/NamingConventionEtoViolation.java
@@ -0,0 +1,13 @@
+package com.devonfw.sample.archunit.task.archunitviolations;
+
+import com.devonfw.sample.archunit.general.common.AbstractEto;
+
+import javax.persistence.MappedSuperclass;
+
+/**
+ * Abstract base class for all persistent entities of this app.
+ */
+@MappedSuperclass
+public abstract class NamingConventionEtoViolation extends AbstractEto {
+
+}
