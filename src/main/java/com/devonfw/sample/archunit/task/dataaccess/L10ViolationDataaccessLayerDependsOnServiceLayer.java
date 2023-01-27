b6a6e8c28cefb2424187eb37dad57a623969e1d9
Vladislav Sehtman <vlad96@hotmail.de>
2023-01-27 12:47:46 +0100

    Refactored L10: Added comment. Added Violation of L12. Dataaccess layer doesnt depend on logic layer.

--- src/main/java/com/devonfw/sample/archunit/task/dataaccess/L10ViolationDataaccessLayerDependsOnServiceLayer.java
+++ src/main/java/com/devonfw/sample/archunit/task/dataaccess/L10ViolationDataaccessLayerDependsOnServiceLayer.java
@@ -5,6 +5,7 @@ import javax.inject.Inject;
 import com.devonfw.sample.archunit.task.service.TaskService;
 
 public class L10ViolationDataaccessLayerDependsOnServiceLayer {
+    // Violation! Dataaccess layer should not depend on service layer.
     @Inject
     TaskService taskService;
 }
