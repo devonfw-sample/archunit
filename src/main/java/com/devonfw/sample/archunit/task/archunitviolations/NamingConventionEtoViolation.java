582bf1b0de9b21b482d02f81dc5550714ed5b447
Jörg Hohwiller <hohwille@users.noreply.github.com>
2023-02-07 17:42:48 +0100

    constructive review

--- src/main/java/com/devonfw/sample/archunit/task/archunitviolations/NamingConventionEtoViolation.java
+++ src/main/java/com/devonfw/sample/archunit/task/archunitviolations/NamingConventionEtoViolation.java
@@ -2,12 +2,9 @@ package com.devonfw.sample.archunit.task.archunitviolations;
 
 import com.devonfw.sample.archunit.general.common.AbstractEto;
 
-import javax.persistence.MappedSuperclass;
-
 /**
  * Abstract base class for all persistent entities of this app.
  */
-@MappedSuperclass
 public abstract class NamingConventionEtoViolation extends AbstractEto {
 
 }
