8cd83b1d26cc28cfb44c5c6f3f0edd1a32eb43f0
Vladislav Sehtman <vlad96@hotmail.de>
2023-02-08 10:30:31 +0100

    Deleted the unnecessary dump and import script. Replaced the import script with a simple class declaration, to demonstrate dependencies towards the batch layer.

--- src/main/java/com/devonfw/sample/archunit/client/L5ViolationClientLayerDependsOnBatchLayer.java
+++ src/main/java/com/devonfw/sample/archunit/client/L5ViolationClientLayerDependsOnBatchLayer.java
@@ -2,10 +2,10 @@ package com.devonfw.sample.archunit.client;
 
 import javax.inject.Inject;
 
-import com.devonfw.sample.archunit.batch.SampleTaskImport;
+import com.devonfw.sample.archunit.batch.SampleBatchClass;
 
 public class L5ViolationClientLayerDependsOnBatchLayer {
     // Violation! Client layer should not depend on batch layer
     @Inject
-    SampleTaskImport taskImport;
+    SampleBatchClass sampleBatchClass;
 }
--- src/main/java/com/devonfw/sample/archunit/task/service/L6ViolationServiceLayerDependsOnBatchLayer.java
+++ src/main/java/com/devonfw/sample/archunit/task/service/L6ViolationServiceLayerDependsOnBatchLayer.java
@@ -2,10 +2,10 @@ package com.devonfw.sample.archunit.task.service;
 
 import javax.inject.Inject;
 
-import com.devonfw.sample.archunit.batch.SampleTaskImport;
+import com.devonfw.sample.archunit.batch.SampleBatchClass;
 
 public class L6ViolationServiceLayerDependsOnBatchLayer {
     // Violation! Service layer should not depend on batch layer
     @Inject
-    SampleTaskImport sampleTaskImport;
+    SampleBatchClass sampleBatchClass;
 }
