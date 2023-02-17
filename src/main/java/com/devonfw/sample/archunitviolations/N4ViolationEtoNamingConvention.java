53b9dd1172957dc9f510f32091dcc074c1a2a29d
zchriste <zina.christensen@capgemini.com>
2023-02-17 10:21:22 +0100

    added violations for the additional naming convention tests N1 and N5

---
+++ src/main/java/com/devonfw/sample/archunitviolations/N4ViolationEtoNamingConvention.java
@@ -0,0 +1,6 @@
+package com.devonfw.sample.archunitviolations;
+
+import com.devonfw.sample.archunit.general.common.AbstractEto;
+
+public class N4ViolationEtoNamingConvention extends AbstractEto {
+}
---
+++ src/main/java/com/devonfw/sample/archunitviolations/N5ViolationUcNamingConvention.java
@@ -0,0 +1,6 @@
+package com.devonfw.sample.archunitviolations;
+
+import com.devonfw.sample.archunit.general.logic.AbstractUc;
+
+public class N5ViolationUcNamingConvention extends AbstractUc {
+}
