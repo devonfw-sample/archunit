599738913c7a61ec6d43e3402a511031c2c7f9b4
Vladislav Sehtman <vlad96@hotmail.de>
2023-02-21 11:51:41 +0100

    Added first draft for C3: Components service layers should not depend on each other.

---
+++ src/test/java/com/devonfw/sample/archunit/ComponentRuleC3LayerService2Service4ComponentTest.java
@@ -0,0 +1,20 @@
+package com.devonfw.sample.archunit;
+
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.lang.ArchRule;
+
+import static com.tngtech.archunit.library.dependencies.SlicesRuleDefinition.slices;
+
+import com.tngtech.archunit.core.importer.ImportOption;
+
+/**
+ * verifying that the service layer of one component does not depend on the service layer of
+ * another component.
+ */
+@AnalyzeClasses(packages = "com.devonfw.sample.archunit", importOptions = ImportOption.DoNotIncludeTests.class)
+public class ComponentRuleC3LayerService2Service4ComponentTest {
+    @ArchTest
+    static final ArchRule no_service_layer_depends_on_service_layer_of_another_component =
+    slices().matching("..archunit.(*).service..").namingSlices("$1 service").should().notDependOnEachOther();
+}
