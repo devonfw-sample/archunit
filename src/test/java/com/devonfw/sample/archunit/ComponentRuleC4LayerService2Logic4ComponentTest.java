845c1307a4625c4845452543eddbd2c43c6b8388
Vladislav Sehtman <vlad96@hotmail.de>
2023-02-27 12:30:48 +0100

    Added new draft for C4Layer2Logic4ComponentTest

--- src/test/java/com/devonfw/sample/archunit/ComponentRuleC4LayerService2Logic4ComponentTest.java
+++ src/test/java/com/devonfw/sample/archunit/ComponentRuleC4LayerService2Logic4ComponentTest.java
@@ -2,15 +2,18 @@ package com.devonfw.sample.archunit;
 
 import com.tngtech.archunit.junit.AnalyzeClasses;
 import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.lang.ArchCondition;
 import com.tngtech.archunit.lang.ArchRule;
-import com.tngtech.archunit.library.dependencies.SliceAssignment;
-import com.tngtech.archunit.library.dependencies.SliceIdentifier;
+import com.tngtech.archunit.lang.ConditionEvents;
+import com.tngtech.archunit.lang.SimpleConditionEvent;
 
-import static com.tngtech.archunit.library.dependencies.SlicesRuleDefinition.slices;
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
 
-import com.tngtech.archunit.core.domain.JavaAccess;
+
+import com.tngtech.archunit.core.domain.Dependency;
 import com.tngtech.archunit.core.domain.JavaClass;
 import com.tngtech.archunit.core.importer.ImportOption;
+
 /**
  * verifying that the service layer of a component may not depend on the
  * logic layer of another component.
@@ -19,49 +22,107 @@ import com.tngtech.archunit.core.importer.ImportOption;
 public class ComponentRuleC4LayerService2Logic4ComponentTest {
 
     static final String PROJECT_NAME = "com.devonfw.sample.archunit";
-    static final String DEFAULT_COMPONENT = PROJECT_NAME + ".general";
-    @ArchTest
-    static final ArchRule no_dependencies_from_componentb_to_any_other_component =
-            slices().assignedFrom(inComponentServiceOrLogicLayer())
-                    .namingSlices("$1")
-                    .should().notDependOnEachOther();
+    static final String DEFAULT_COMPONENT_SIMPLE_NAME = "general";
+    static final String DEFAULT_COMPONENT_FULL_NAME = PROJECT_NAME + "." + DEFAULT_COMPONENT_SIMPLE_NAME;
 
-    @ArchTest
-    static final ArchRule no_cycles_by_method_calls_between_slices =
-            slices().matching("..archunit.(*).(service)..").namingSlices("$2 of $1").should().notDependOnEachOther();
+    static ArchCondition<JavaClass> haveNonCompliantComponentDependencies = new ArchCondition<JavaClass> ("have dependencies, towards another components logic layer (Rule-C4).") {
+        @Override
+        public void check(JavaClass sourceClass, ConditionEvents events) {
+            String sourceClassName = sourceClass.getFullName();
+            String sourceClassLayer = getClassLayer(sourceClass);
+            String sourceClassComponent = getComponentNameOfClass(sourceClass);
 
-    private static SliceAssignment inComponentServiceOrLogicLayer() {
-        return new SliceAssignment() {
-            @Override
-            public String getDescription() {
-                return "the service layer of one component and a different components logic layer";
-            }
+            // All project components service layer except the default component.
+            if(sourceClassLayer.equals("service") && !sourceClassComponent.equals("")  && !sourceClassComponent.equals(DEFAULT_COMPONENT_SIMPLE_NAME)){
+                for (Dependency dependency : sourceClass.getDirectDependenciesFromSelf()) {
+                    JavaClass targetClass = dependency.getTargetClass();
+                    String targetClassName = targetClass.getFullName();
+                    String targetClassComponent = getComponentNameOfClass(targetClass);
+                    String targetClassLayer = getClassLayer(targetClass);
+                    boolean dependencyAllowed = isAllowedDependency(sourceClass, targetClass);
 
-            @Override
-            public SliceIdentifier getIdentifierOf(JavaClass javaClass) {
-                String sourcePackageName = javaClass.getPackageName();
-                // All components except the default component.
-                if(sourcePackageName.contains(PROJECT_NAME) && !sourcePackageName.contains(DEFAULT_COMPONENT)){
-                    for (JavaAccess<?> access : javaClass.getAccessesFromSelf()) {
-                        String targetPackageName = access.getTargetOwner().getName();
-                        if (sourcePackageName.contains(".service") && !targetPackageName.contains(".logic")) {
-                            String componentName = javaClass.getPackageName().substring(PROJECT_NAME.length() + 1, sourcePackageName.length()).replace(".service", "");
-                            if(!targetPackageName.contains(componentName)){
-                                return SliceIdentifier.of(componentName);
-                            }
-                        }
-                        if(!sourcePackageName.contains("service") && targetPackageName.contains(".logic")){
-                            String componentName = javaClass.getPackageName().substring(PROJECT_NAME.length() + 1, sourcePackageName.length()).replace(".logic", "");
-                            if(!targetPackageName.contains(componentName)) {
-                                return SliceIdentifier.of(componentName);
-                            }
-                            
-                        }
+                    // WARNING: Dependency of a components service layer towards another components layer other than logic wont be registered as a violation. 
+                    // (Other rules cover these violations.)
+                    if(targetClassName.startsWith(PROJECT_NAME) && targetClassLayer.equals("logic") && !dependencyAllowed) {
+                        String message = String.format("Code from service layer of a component shall not depend on logic layer of a different component. ('%s.%s' is dependend on '%s.%s'. Dependency to (%s). Violated in: (%s)", sourceClassComponent, sourceClassLayer, targetClassComponent, targetClassLayer, targetClass.getDescription(), sourceClassName);
+                        events.add(new SimpleConditionEvent(sourceClass, true, message));
                     }
                 }
-
-                return SliceIdentifier.ignore();
             }
-        };
+        }
+    };
+    
+    @ArchTest
+    static final ArchRule no_dependencies_from_a_components_service_layer_to_anothers_component_logic_layer = 
+        noClasses()
+        .should(haveNonCompliantComponentDependencies)
+        .allowEmptyShould(true);
+
+    /**
+     * Dependency of a components service layer towards the same components logic or common layer is allowed.
+     */
+    private static boolean isAllowedDependency(JavaClass sourceClass, JavaClass targetClass) {
+        boolean isAllowed = false;
+        String targetClassName = targetClass.getFullName();
+
+        // Components service layer can depend on their own lower layers: logic and common.
+        if(classesAreInSameComponent(sourceClass, targetClass) && (targetClassName.contains("logic") || targetClassName.contains("common"))) {
+            isAllowed = true;
+        }
+        // Components may always depend on the default business component.
+        if(targetClassName.startsWith(DEFAULT_COMPONENT_FULL_NAME) && (targetClassName.contains("logic") || targetClassName.contains("common"))) {
+            isAllowed = true;
+        }
+        return isAllowed;
     }
+
+    /**
+     * Returns the name of the layer of a given class, if the class does lie in a projects component.
+     * Otherwise returns a blank string "".
+     */
+    private static String getClassLayer(JavaClass clazz) {
+        String classLayerName = "";
+        String className = clazz.getFullName();
+        if(className.startsWith(PROJECT_NAME)) {
+            String classPackageName = clazz.getPackageName();
+            String classComponentAndLayerName = classPackageName.substring(PROJECT_NAME.length() + 1, classPackageName.length());
+            int beginOfLayerName = classComponentAndLayerName.indexOf(".") + 1;
+            int endOfLayerName = classComponentAndLayerName.length();
+            classLayerName = classComponentAndLayerName.substring(beginOfLayerName, endOfLayerName);
+        }
+        return classLayerName;
+    }
+
+    /**
+     * Returns the name of the component of a given class, if the class does lie in a projects component.
+     * Otherwise returns a blank string "".
+     */
+    private static String getComponentNameOfClass(JavaClass clazz) {
+        String classComponentName = "";
+        String className = clazz.getFullName();
+        if(className.startsWith(PROJECT_NAME)) {
+            String classPackageName = clazz.getPackageName();
+            String classComponentAndLayerName = classPackageName.substring(PROJECT_NAME.length() + 1, classPackageName.length());
+            int endOfComponentName = classComponentAndLayerName.indexOf(".");
+            classComponentName = classComponentAndLayerName.substring(0, endOfComponentName);
+        }
+        return classComponentName;
+    }
+
+    /**
+     * Returns true, if both given classes lie in the same projects component.
+     * In case at least one of the given classes does not lie in any of the project components false will be returned.
+     * @param sourceClass JavaClass A to check against another JavaClass B.
+     * @param targetClass JavaClass B to check against another JavaClass A.
+     */
+    private static boolean classesAreInSameComponent(JavaClass sourceClass, JavaClass targetClass) {
+        String sourceComponent = getComponentNameOfClass(sourceClass);
+        String targetComponent = getComponentNameOfClass(targetClass);
+        if( targetComponent == "" || sourceComponent =="")
+        {
+            return false;
+        }
+        return sourceComponent.equals(targetComponent) ? true : false;
+    }
+
 }
