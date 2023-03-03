b0566421a996a4c0d347fc4e2006735bac042f90
Vladislav Sehtman <vlad96@hotmail.de>
2023-03-03 11:47:02 +0100

    Added C3 and C4 rule violations

---
+++ src/main/java/com/devonfw/sample/archunit/componentb/logic/C4PositiveTest.java
@@ -0,0 +1,5 @@
+package com.devonfw.sample.archunit.componentb.logic;
+
+public class C4PositiveTest {
+    //Demo class. C4ViolationServiceLayerOfComponentbDependsOnLogicLayer class can depend on this class.
+}
