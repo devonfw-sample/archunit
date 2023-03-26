82e8cbaaa1a40868cdcf23c68a2a8742e8397a59
Nicolas van Bellen <nicolas.van.bellen@rwth-aachen.de>
2023-03-26 10:43:31 +0200

    fix - restructure of the batch component

---
+++ src/main/java/com/devonfw/sample/archunit/componentA/batch/C7ViolationBatchLayerOfBatchDependsOnLogicLayerOfComponentTask.java
@@ -0,0 +1,7 @@
+package com.devonfw.sample.archunit.componenta.batch;
+
+import com.devonfw.sample.archunit.task.logic.UcDeleteTaskItem;
+
+public class C7ViolationBatchLayerOfBatchDependsOnLogicLayerOfComponentTask {
+    UcDeleteTaskItem ucDeleteTaskItem;
+}
