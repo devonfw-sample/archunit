ffef215ff2aff467a5031eb3d79f170fc6af5b7d
Jörg Hohwiller <hohwille@users.noreply.github.com>
2023-04-25 08:07:47 +0200

    Update ApplicationPersistenceEntity.java

--- src/main/java/com/devonfw/sample/archunit/violation/common/ApplicationPersistenceEntity.java
+++ src/main/java/com/devonfw/sample/archunit/violation/common/ApplicationPersistenceEntity.java
@@ -7,10 +7,8 @@ import com.devonfw.sample.archunit.general.common.ApplicationEntity;
 /**
  * Abstract base class for all persistent entities of this app.
  */
+// Violation: ApplicationPersistenceEntity has to be be in layer dataaccess
 @MappedSuperclass
 public abstract class ApplicationPersistenceEntity implements ApplicationEntity {
-  /*
-    ApplicationPersistenceEntity have to have be in layer dataaccess
-  */
 
 }
