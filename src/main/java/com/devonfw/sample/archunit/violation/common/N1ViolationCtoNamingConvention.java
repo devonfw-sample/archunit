3799888654a83c469a3fc4ddef540779121d0a5a
Jörg Hohwiller <hohwille@users.noreply.github.com>
2023-04-25 08:29:37 +0200

    Update and rename src/main/java/com/devonfw/sample/archunitviolations/N1ViolationCtoNamingConvention.java to src/main/java/com/devonfw/sample/archunit/violation/common/N1ViolationCtoNamingConvention.java

--- src/main/java/com/devonfw/sample/archunit/violation/common/N1ViolationCtoNamingConvention.java
+++ src/main/java/com/devonfw/sample/archunit/violation/common/N1ViolationCtoNamingConvention.java
@@ -1,6 +1,7 @@
-package com.devonfw.sample.archunitviolations;
+package com.devonfw.sample.archunit.violation.common;
 
 import com.devonfw.sample.archunit.general.common.AbstractCto;
 
+// Violation: CTO does not end with "Cto".
 public class N1ViolationCtoNamingConvention extends AbstractCto {
 }
