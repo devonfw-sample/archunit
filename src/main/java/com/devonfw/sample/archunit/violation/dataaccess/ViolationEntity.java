81a5d1c71321b47db0c2f37a3c1e8c5e164d6ada
Jörg Hohwiller <hohwille@users.noreply.github.com>
2023-04-25 08:17:03 +0200

    Update ViolationEntity.java

--- src/main/java/com/devonfw/sample/archunit/violation/dataaccess/ViolationEntity.java
+++ src/main/java/com/devonfw/sample/archunit/violation/dataaccess/ViolationEntity.java
@@ -10,74 +10,12 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import com.devonfw.sample.archunit.general.dataaccess.ApplicationPersistenceEntity;
-import com.devonfw.sample.archunit.task.common.TaskItem;
+import com.devonfw.sample.archunit.violation.common.ViolationItem;
 
 /**
  * {@link TaskItem} implementation as {@link ApplicationPersistenceEntity}.
  */
 @Entity
-@Table(name = "TASK_ITEM")
-public class ViolationEntity extends ApplicationPersistenceEntity implements TaskItem {
-
-  @Override
-  public String getTitle() {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'getTitle'");
-  }
-
-  @Override
-  public void setTitle(String title) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'setTitle'");
-  }
-
-  @Override
-  public boolean isCompleted() {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'isCompleted'");
-  }
-
-  @Override
-  public void setCompleted(boolean completed) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'setCompleted'");
-  }
-
-  @Override
-  public boolean isStarred() {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'isStarred'");
-  }
-
-  @Override
-  public void setStarred(boolean starred) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'setStarred'");
-  }
-
-  @Override
-  public LocalDateTime getDeadline() {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'getDeadline'");
-  }
-
-  @Override
-  public void setDeadline(LocalDateTime deadline) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'setDeadline'");
-  }
-
-  @Override
-  public Long getTaskListId() {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'getTaskListId'");
-  }
-
-  @Override
-  public void setTaskListId(Long taskListId) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'setTaskListId'");
-  }
-
- 
+@Table(name = "VIOLATION")
+public class ViolationEntity extends ApplicationPersistenceEntity implements ViolationItem {
 }
