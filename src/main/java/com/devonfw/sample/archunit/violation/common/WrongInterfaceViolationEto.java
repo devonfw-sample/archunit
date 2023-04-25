6fddd89bd89932a2da8ce2f984e714f9cb65152f
Jörg Hohwiller <hohwille@users.noreply.github.com>
2023-04-25 08:11:32 +0200

    Update WrongInterfaceViolationEto.java

--- src/main/java/com/devonfw/sample/archunit/violation/common/WrongInterfaceViolationEto.java
+++ src/main/java/com/devonfw/sample/archunit/violation/common/WrongInterfaceViolationEto.java
@@ -6,6 +6,6 @@ import com.devonfw.sample.archunit.general.common.AbstractEto;
 /**
  * {@link ViolationItem} implementation as {@link AbstractEto}.
  */
- // Violation: ETO for ViolationItem interface shall be named ViolationItemEto
+ // Violation: ETO for entity interface shall be named «EntityName»Eto (ViolationItemEto)
 public class WrongInterfaceViolationEto extends AbstractEto implements ViolationItem {
 }
