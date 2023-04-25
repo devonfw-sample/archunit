251ed7eaba189f5ff9e3b2271529806c81411dda
Jörg Hohwiller <hohwille@users.noreply.github.com>
2023-04-25 10:35:46 +0200

    Update ThirdPartyDataMappingPersistanceOk.java

--- src/main/java/com/devonfw/sample/archunit/general/dataaccess/violations/ThirdPartyDataMappingPersistanceOk.java
+++ src/main/java/com/devonfw/sample/archunit/general/dataaccess/violations/ThirdPartyDataMappingPersistanceOk.java
@@ -3,6 +3,8 @@ package com.devonfw.sample.archunit.general.dataaccess.violations;
 import javax.persistence.Converter; // compliant
 
 @Converter
+// OK - just for demonstration purpose. A reasonable example can be found here:
+// https://github.com/devonfw/devon4j/blob/master/documentation/guide-jpa.asciidoc#entities-and-datatypes
 public class ThirdPartyDataMappingPersistanceOk {
 
 }
