d9e0b8a1cd8bd64d5ab5183d80a55782e277fa1a
Jörg Hohwiller <hohwille@users.noreply.github.com>
2023-04-25 10:44:18 +0200

    Update L2ViolationNoLayerOtherThanClientDependsOnClientLayer.java

--- src/main/java/com/devonfw/sample/archunit/task/service/L2ViolationNoLayerOtherThanClientDependsOnClientLayer.java
+++ src/main/java/com/devonfw/sample/archunit/task/service/L2ViolationNoLayerOtherThanClientDependsOnClientLayer.java
@@ -5,7 +5,7 @@ import javax.inject.Inject;
 import com.devonfw.sample.archunit.client.SampleClient;
 
 public class L2ViolationNoLayerOtherThanClientDependsOnClientLayer {
-    // Violation! No layer should depend on client layer 
+    // Violation: service layer may not depend on client layer.
     @Inject
     SampleClient sampleClient;
 }
