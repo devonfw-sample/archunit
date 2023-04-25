c6f88341e4755bcb17744827dd73dc2d90d48c62
Jörg Hohwiller <hohwille@users.noreply.github.com>
2023-04-25 10:33:40 +0200

    Update and rename src/main/java/com/devonfw/sample/archunitviolations/N5ViolationUcNamingConvention.java to src/main/java/com/devonfw/sample/archunit/violation/logic/N5ViolationUcNamingConvention.java

--- src/main/java/com/devonfw/sample/archunit/violation/logic/N5ViolationUcNamingConvention.java
+++ src/main/java/com/devonfw/sample/archunit/violation/logic/N5ViolationUcNamingConvention.java
@@ -2,5 +2,6 @@ package com.devonfw.sample.archunitviolations;
 
 import com.devonfw.sample.archunit.general.logic.AbstractUc;
 
+// Violation: Use-case shall start with "Uc" prefix.
 public class N5ViolationUcNamingConvention extends AbstractUc {
 }
