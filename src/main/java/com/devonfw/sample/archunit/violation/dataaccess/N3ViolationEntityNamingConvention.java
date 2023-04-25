c6a0d4a6073d1127564fc01ca16baf8dd931492e
Jörg Hohwiller <hohwille@users.noreply.github.com>
2023-04-25 08:47:24 +0200

    Update and rename src/main/java/com/devonfw/sample/archunitviolations/N3ViolationEntityNamingConvention.java to src/main/java/com/devonfw/sample/archunit/violation/dataaccess/N3ViolationEntityNamingConvention.java

--- src/main/java/com/devonfw/sample/archunit/violation/dataaccess/N3ViolationEntityNamingConvention.java
+++ src/main/java/com/devonfw/sample/archunit/violation/dataaccess/N3ViolationEntityNamingConvention.java
@@ -1,6 +1,7 @@
-package com.devonfw.sample.archunitviolations;
+package com.devonfw.sample.archunit.violation.dataaccess;
 
 import com.devonfw.sample.archunit.general.dataaccess.ApplicationPersistenceEntity;
 
+// Violation: Entity does not end with "Entity".
 public class N3ViolationEntityNamingConvention extends ApplicationPersistenceEntity {
 }
