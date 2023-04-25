dee623d986b8c50e18ee6f4e4b67ad5f6dcff427
Jörg Hohwiller <hohwille@users.noreply.github.com>
2023-04-25 10:31:46 +0200

    Update and rename src/main/java/com/devonfw/sample/archunitviolations/N4ViolationEtoNamingConvention.java to src/main/java/com/devonfw/sample/archunit/violation/common/N4ViolationEtoNamingConvention.java

--- src/main/java/com/devonfw/sample/archunit/violation/common/N4ViolationEtoNamingConvention.java
+++ src/main/java/com/devonfw/sample/archunit/violation/common/N4ViolationEtoNamingConvention.java
@@ -2,5 +2,6 @@ package com.devonfw.sample.archunitviolations;
 
 import com.devonfw.sample.archunit.general.common.AbstractEto;
 
+// Violation: ETO should have suffix "Eto".
 public class N4ViolationEtoNamingConvention extends AbstractEto {
 }
